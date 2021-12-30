b1c4c89ff148174ef709b44d4c6fb24a64dceec9
wiktoriakeller <wiktoriakeller552@gmail.com>
2021-12-30 19:27:07 +0100

    Add init files

---
+++ HangmanServer/HangmanServer.cpp
@@ -0,0 +1,12 @@
+﻿// HangmanServer.cpp : Defines the entry point for the application.
+//
+
+#include "HangmanServer.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
